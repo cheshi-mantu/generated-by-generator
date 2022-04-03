0e9056a50b4b1b4c334793c5b11f6d8a4a9aae60
Daniil Salokin <work.salokin@gmail.com>
2022-04-03 22:30:50 +0300

    Create docs for AllureStepTest.class

--- src/test/java/io/qameta/examples/AllureStepTest.java
+++ src/test/java/io/qameta/examples/AllureStepTest.java
@@ -6,13 +6,37 @@ import org.junit.jupiter.api.Test;
 
 public class AllureStepTest {
 
+    /**
+        AllureStepTest.class
+    This class shows what actions Allure allows to do with the steps.
+
+    1) GLOBAL_PARAMETER is a global variable which can be used inside AllureStepTest class.
+
+    2) annotatedStepTest() - a test that contains the steps described in (5) and the passing parameter
+    is a "local value" that can only be used within that test.
+
+    3) lambdaStepTest() is a test which shows how Allure handles lambda steps.
+    Here we see that the variable localParameter is set inside the test. This is used in the name
+    of the "parent" step and the first nested step. The second nested step uses GLOBAL_PARAMETER inside,
+    is a lambda step and can also have nested steps.
+
+    4) annotatedStep() is a step which can take a parameter as shown in (1) and contain nested steps.
+
+    5) nestedAnnotatedStep() - step which takes a GLOBAL_PARAMETER value and passes it in its name.
+
+    If we run tests (2) and (3), we can see how Allure Report reproduces them.
+     */
+
+    // (1) GLOBAL_PARAMETER
     private static final String GLOBAL_PARAMETER = "global value";
 
+    // (2) annotatedStepTest
     @Test
     public void annotatedStepTest() {
         annotatedStep("local value");
     }
 
+    // (3) lambdaStepTest
     @Test
     public void lambdaStepTest() {
         final String localParameter = "parameter value";
@@ -22,11 +46,13 @@ public class AllureStepTest {
         });
     }
 
+    // (4) annotatedStep
     @Step("Parent annotated step with parameter [{parameter}]")
     public void annotatedStep(final String parameter) {
         nestedAnnotatedStep();
     }
 
+    // (5) nestedAnnotatedStep
     @Step("Nested annotated step with global parameter [{this.GLOBAL_PARAMETER}]")
     public void nestedAnnotatedStep() {
 
