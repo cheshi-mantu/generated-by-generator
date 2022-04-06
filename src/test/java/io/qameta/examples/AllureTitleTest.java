69127b1f96f42fd5b20d8b47990921694752a239
Daniil Salokin <work.salokin@gmail.com>
2022-04-06 18:24:37 +0300

    TitleTest docs added

--- src/test/java/io/qameta/examples/AllureTitleTest.java
+++ src/test/java/io/qameta/examples/AllureTitleTest.java
@@ -5,9 +5,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
+ * Titles are used to name the test case.
  *
+ * 1) Annotation Title Test
+ * 2) Dynamic Title Test
  *
-*/
+ * 1) Annotation Title Test
+ *    Allows you to set the test case title statically.
+ *    See a screenshot here: ###allure-report-documentation####/annotated-title
+ *    Pros: your code looks cleaner
+ *    Cons: no way to assign values used in other test cases
+ *
+ * 2) Dynamic Title Test
+ *    Allows dynamically change the test case title from the code.
+ *    See a screenshot here: ###allure-report-documentation##/dynamic-title
+ *    Pros: your code looks more complex
+ *    Cons: more options to change a large number of test cases quickly and efficiently
+ */
 
 public class AllureTitleTest {
     @Test
