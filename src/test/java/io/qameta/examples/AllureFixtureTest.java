4a443c35ddd17c4820193123d8bc2388a970ac18
Daniil Salokin <work.salokin@gmail.com>
2022-04-10 21:39:44 +0300

    Add docs in AllureFixtureTest

--- src/test/java/io/qameta/examples/AllureFixtureTest.java
+++ src/test/java/io/qameta/examples/AllureFixtureTest.java
@@ -6,6 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static io.qameta.allure.Allure.step;
 
+/**
+ * Fixtures are general actions to be taken before or after the test steps.
+ *
+ * This example shows how fixtures can be specified by annotations.
+ * See a screenshot here: ###allure-report-documentation##/fixtures
+ */
+
+
 public class AllureFixtureTest {
     @BeforeAll
     public static void beforeAllMethods() {
